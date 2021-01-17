68cb97f4d4fc1cc428fe8b8d3da069198f514067
Edenilson Fonseca Chagas <edenilsonfc@gmail.com>
2021-01-17 17:01:49 -0300

    FindById

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-insert into tb_category (name) values ('Boos');
+insert into tb_category (name) values ('Books');
 insert into tb_category (name) values ('Electonics');
-insert into tb_category (name) values ('Computes');
+insert into tb_category (name) values ('Computers');
