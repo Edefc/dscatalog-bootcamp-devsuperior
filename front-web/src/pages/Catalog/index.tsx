b137e905d858299b766a119032eb2dc3b9852441
Edenilson Fonseca Chagas <edenilsonfc@gmail.com>
2021-01-26 18:51:56 -0300

    Finalizing the product listing layout

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ProductCard from './components/ProductCard';
 import './styles.scss';
 
 const Catalog = () => (
@@ -7,7 +8,14 @@ const Catalog = () => (
             Catálago de produtos
         </h1>
         <div className="catalog-products">
-
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        
         </div>
     </div>
 
