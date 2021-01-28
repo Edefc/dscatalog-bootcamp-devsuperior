4fed74bf8da2af111c1f860524c57ca822c42b1f
Edenilson Fonseca Chagas <edenilsonfc@gmail.com>
2021-01-28 15:02:09 -0300

    Creating http request

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { makeRequest } from '../../core/utils/request';
 import ProductCard from './components/ProductCard';
 import './styles.scss';
 
@@ -9,8 +10,12 @@ const Catalog = () => {
     //um estado no componente, e listar os produtos dinâmicamente
 
     useEffect (() => {
-        fetch('http://localhost:3000/products')
-        .then(response => response.json())
+        const params = {
+            page: 0,
+            linesPerPage: 12
+
+        }
+        makeRequest({ url: '/products', params })
         .then(response => console.log(response));
 
     }, []);
