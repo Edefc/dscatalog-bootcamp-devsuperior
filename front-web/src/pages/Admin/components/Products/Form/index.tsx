2816af3ce3d33244daaddee955933e768a71fb50
Edenilson Fonseca Chagas <edenilsonfc@gmail.com>
2021-02-03 15:29:02 -0300

    Registering product on the front-web 03-13

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -1,3 +1,4 @@
+import { makeRequest } from 'core/utils/request';
 import React, { useState } from 'react';
 import BaseForm from '../../BaseForm';
 import './styles.scss';
@@ -6,30 +7,42 @@ type FormState = {
     name: string;
     category: string;
     price: string;
-   
+    description: string;
 }
+type FormEvent = React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>;
+
 
 const Form = () => {
     const [formData, setFormData] = useState<FormState>({
-       name: 'Computador',
-       category: 'eletrônicos',
-       price: ''
+        name: '',
+        category: '',
+        price: '',
+        description: ''
 
     });
 
 
-    const handleOnChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-       const name = event.target.name;
+    const handleOnChange = (event: FormEvent) => {
+        const name = event.target.name;
         const value = event.target.value;
-        setFormData(data => ( {...data, [name]: value }));
-       // console.log({name, value});
-        
+        setFormData(data => ({ ...data, [name]: value }));
+        // console.log({name, value});
+
     }
 
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        console.log(formData);
+        const payload = {
+            ...formData,
+            imgUrl: 'https://www.jbl.com.br/dw/image/v2/AAUJ_PRD/on/demandware.static/-/Sites-masterCatalog_Harman/default/dw198a7d47/JBLWind2_1605x1605_HERO.png?sw=537&sfrm=png',
+            categories: [{ id: formData.category }]
+        }
+        makeRequest({ url: '/products', method: 'POST', data: payload })
+            .then(() => {
+                setFormData({ name: '', category: '', price: '', description: '' });
+            });
+
 
     }
 
@@ -39,7 +52,7 @@ const Form = () => {
                 <div className="row">
                     <div className="col-6">
                         <input
-                        value={formData.name}
+                            value={formData.name}
                             name="name"
                             type="text"
                             className="form-control mb-5"
@@ -48,24 +61,33 @@ const Form = () => {
                         />
 
                         <select
-                        value={formData.category}
+                            value={formData.category}
                             name="category"
                             className="form-control mb-5"
                             onChange={handleOnChange}
                         >
-                            <option value="livros">Livros</option>
-                            <option value="computadores">Computadores</option>
-                            <option value="eletrônicos">Eletrônicos</option>
+                            <option value="1">Livros</option>
+                            <option value="3">Computadores</option>
+                            <option value="2">Eletrônicos</option>
                         </select>
 
                         <input
-                        value={formData.price}
+                            value={formData.price}
                             name="price"
                             type="text"
                             className="form-control mb-5"
                             onChange={handleOnChange}
                             placeholder='Price'
                         />
+                    </div>
+                    <div className="col-6">
+                        <textarea
+                            name="description"
+                            value={formData.description}
+                            onChange={handleOnChange}
+                            className="form-control"
+                            cols={30}
+                            rows={10} />
                     </div>
                 </div>
             </BaseForm>
