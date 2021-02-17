3680954fd1aede73b48cf33cc6a2ce472b53ce71
Edenilson Fonseca Chagas <edenilsonfc@gmail.com>
2021-02-17 19:30:45 -0300

    User CRUD part 1 04-04

---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/UserService.java
@@ -0,0 +1,103 @@
+package com.devsuperior.dscatalog.services;
+
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dscatalog.dto.CategoryDTO;
+import com.devsuperior.dscatalog.dto.RoleDTO;
+import com.devsuperior.dscatalog.dto.UserDTO;
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.entities.Role;
+import com.devsuperior.dscatalog.entities.User;
+import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.repositories.RoleRepository;
+import com.devsuperior.dscatalog.repositories.UserRepository;
+import com.devsuperior.dscatalog.services.exception.DataBaseException;
+import com.devsuperior.dscatalog.services.exception.ResourceNotFoundException;
+
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	@Autowired
+	private RoleRepository  roleRepository;
+
+	@Transactional(readOnly = true)
+	public Page<UserDTO> findAllPaged(PageRequest pageRequest) {
+		Page<User> list = repository.findAll(pageRequest);
+		return list.map(x -> new UserDTO(x));
+	}
+
+	@Transactional(readOnly = true)
+	public UserDTO findById(Long id) {
+		try {
+			Optional<User> obj = repository.findById(id);
+			User entity = obj.orElseThrow(() -> new EntityNotFoundException("User not found"));
+			return new UserDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Not found Id: " + id);
+
+		}
+
+	}
+
+	@Transactional
+	public UserDTO insert(UserDTO dto) {
+		User entity = new User();
+		copyDtoToEntiry(dto, entity);
+		// entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new UserDTO(entity);
+	}
+
+	@Transactional
+	public UserDTO update(Long id, UserDTO dto) {
+		try {
+			User entity = repository.getOne(id);
+			copyDtoToEntiry(dto, entity);
+			// entity.setName(dto.getName());
+			entity = repository.save(entity);
+			return new UserDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Not found Id:  " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Not found Id: " + id);
+		}
+
+		catch (DataIntegrityViolationException e) {
+			throw new DataBaseException("Integrity violation");
+		}
+
+	}
+
+	private void copyDtoToEntiry(UserDTO dto, User entity) {
+		
+		entity.setFirstName(dto.getFirstName());
+		entity.setLastName(dto.getLastName());
+		entity.setEmail(dto.getEmail());
+		
+		
+		entity.getRoles().clear();
+		for (RoleDTO roleDto : dto.getRoles()) {
+			Role role = roleRepository.getOne(roleDto.getId());
+			entity.getRoles().add(role);
+		}
+
+	}
+
+}
